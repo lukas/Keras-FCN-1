0c80971fb31e1538ca925dd57f34f4389a81e4c8
lukas <lukeab@gmail.com>
2017-08-15 14:30:34 -0800

    Conform to s2k data format

--- train.py
+++ train.py
@@ -11,18 +11,35 @@ from keras.metrics import binary_accuracy
 from keras.models import load_model
 import keras.backend as K
 #import keras.utils.visualize_util as vis_util
-
+from inference import inference_model
 from models import *
 from utils.loss_function import *
 from utils.metrics import *
 from utils.SegDataGenerator import *
 import time
+import argparse
+import wandb
+import keras
+
+parser = argparse.ArgumentParser(description='Run training')
+parser.add_argument('name', help='name of the run')
+
+parser.add_argument("--load-model", help="load previously saved h5 model")
+
+args = parser.parse_args()
+run_name = args.name
+
+conf = wandb.Config(args)
+
+wandb.sync(run_name)
+
+
 
 
 def train(batch_size, epochs, lr_base, lr_power, weight_decay, classes,
           model_name, train_file_path, val_file_path,
           data_dir, label_dir, target_size=None, batchnorm_momentum=0.9,
-          resume_training=False, class_weight=None, dataset='VOC2012',
+          load_model=False, class_weight=None, dataset='VOC2012',
           loss_fn = softmax_sparse_crossentropy_ignoring_last_label,
           metrics = [sparse_accuracy_ignoring_last_label],
           loss_shape=None,
@@ -38,9 +55,13 @@ def train(batch_size, epochs, lr_base, lr_power, weight_decay, classes,
 
     ###########################################################
     current_dir = os.path.dirname(os.path.realpath(__file__))
-    save_path = os.path.join(current_dir, 'Models/' + model_name)
+    #save_path = os.path.join(current_dir, 'Models/' + model_name)
+    save_path = os.path.join(current_dir, run_name)
     if os.path.exists(save_path) is False:
         os.mkdir(save_path)
+
+    with open(os.path.join(save_path, 'conf.json'), 'w') as outfile:
+        json.dump(conf, outfile)
 
     # ###############learning rate scheduler####################
     def lr_scheduler(epoch, mode='power_decay'):
@@ -85,11 +106,48 @@ def train(batch_size, epochs, lr_base, lr_power, weight_decay, classes,
     optimizer = SGD(lr=lr_base, momentum=0.9)
     # optimizer = Nadam(lr=lr_base, beta_1 = 0.825, beta_2 = 0.99685)
 
+
+    csv_logger = CSVLogger(
+            #'output/{}_fcn_vgg16.csv'.format(datetime.datetime.now().isoformat()))
+            '%s/tmp_fcn_vgg16_cosmic_small.csv' % run_name)
+
+
+    with open(conf.test_file_path) as f:
+        test_image_list = f.read().splitlines()
+        
+    print(test_image_list)
+    
+    class SaveSampleImages(keras.callbacks.Callback):
+        def on_train_begin(self, logs={}):
+            self.batchnum=0
+                
+        def on_epoch_end(self, batch, logs={}):
+            directory = '%s/samples/%i' % (run_name, self.batchnum)
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            model_name = 'AtrousFCN_Resnet50_16s'
+            #model_name = 'Atrous_DenseNet'
+            #model_name = 'DenseNet_FCN'
+            weight_file = 'checkpoint_weights.hdf5'
+            image_size = (320, 320)
+            data_dir        = os.path.expanduser('~/.keras/datasets/VOC2012/VOCdevkit/VOC2012/JPEGImages')
+            label_dir       = os.path.expanduser('~/.keras/datasets/VOC2012/VOCdevkit/VOC2012/SegmentationClass')
+
+
+          
+            results = inference_model(self.model, image_size, test_image_list, data_dir, label_dir, run_name)
+                                    
+            for image, result in zip(image_list, results):                
+                result.save('%s/%s.png' % (directory, image))
+                        
+            self.batchnum+=1
+
+    
     model.compile(loss=loss_fn,
                   optimizer=optimizer,
                   metrics=metrics)
-    if resume_training:
-        model.load_weights(checkpoint_path, by_name=True)
+    if load_model:
+        model.load_weights(load_model)
     model_path = os.path.join(save_path, "model.json")
     # save model structure
     f = open(model_path, 'w')
@@ -111,8 +169,15 @@ def train(batch_size, epochs, lr_base, lr_power, weight_decay, classes,
         tensorboard = TensorBoard(log_dir=os.path.join(save_path, 'logs'), histogram_freq=10, write_graph=True)
         callbacks.append(tensorboard)
     # ################### checkpoint saver#######################
+
+    callbacks.append(csv_logger)
+    
     checkpoint = ModelCheckpoint(filepath=os.path.join(save_path, 'checkpoint_weights.hdf5'), save_weights_only=True)#.{epoch:d}
     callbacks.append(checkpoint)
+
+    save_sample_images = SaveSampleImages()
+    callbacks.append(save_sample_images)
+    
     # set data generator and train
     train_datagen = SegDataGenerator(zoom_range=[0.5, 2.0],
                                      zoom_maintain_shape=True,
@@ -133,7 +198,7 @@ def train(batch_size, epochs, lr_base, lr_power, weight_decay, classes,
         fp.close()
         return len(lines)
     history = model.fit_generator(
-        generator=train_datagen.flow_from_directory(
+        train_datagen.flow_from_directory(
             file_path=train_file_path,
             data_dir=data_dir, data_suffix=data_suffix,
             label_dir=label_dir, label_suffix=label_suffix,
@@ -142,12 +207,13 @@ def train(batch_size, epochs, lr_base, lr_power, weight_decay, classes,
             batch_size=batch_size, shuffle=True,
             loss_shape=loss_shape,
             ignore_label=ignore_label,
+
             # save_to_dir='Images/'
         ),
-        steps_per_epoch=get_file_len(train_file_path),
+        get_file_len(train_file_path),
         epochs=epochs,
         callbacks=callbacks,
-        nb_worker=4,
+        workers=4,
         # validation_data=val_datagen.flow_from_directory(
         #     file_path=val_file_path, data_dir=data_dir, data_suffix='.jpg',
         #     label_dir=label_dir, label_suffix='.png',classes=classes,
@@ -164,51 +230,51 @@ if __name__ == '__main__':
     model_name = 'AtrousFCN_Resnet50_16s'
     #model_name = 'Atrous_DenseNet'
     #model_name = 'DenseNet_FCN'
-    batch_size = 16
+    
     batchnorm_momentum = 0.95
-    epochs = 250
-    lr_base = 0.01 * (float(batch_size) / 16)
+    
+    lr_base = 0.01 * (float(conf.batch_size) / 16)
     lr_power = 0.9
     resume_training = False
-    if model_name is 'AtrousFCN_Resnet50_16s':
+    if conf.model_name is 'AtrousFCN_Resnet50_16s':
         weight_decay = 0.0001/2
     else:
         weight_decay = 1e-4
     target_size = (320, 320)
-    dataset = 'VOC2012_BERKELEY'
-    if dataset == 'VOC2012_BERKELEY':
+    #dataset = 'VOC2012_BERKELEY'
+#    if dataset == 'VOC2012_BERKELEY':
         # pascal voc + berkeley semantic contours annotations
-        train_file_path = os.path.expanduser('~/.keras/datasets/VOC2012/combined_imageset_train.txt') #Data/VOClarge/VOC2012/ImageSets/Segmentation
-        # train_file_path = os.path.expanduser('~/.keras/datasets/oneimage/train.txt') #Data/VOClarge/VOC2012/ImageSets/Segmentation
-        val_file_path   = os.path.expanduser('~/.keras/datasets/VOC2012/combined_imageset_val.txt')
-        data_dir        = os.path.expanduser('~/.keras/datasets/VOC2012/VOCdevkit/VOC2012/JPEGImages')
-        label_dir       = os.path.expanduser('~/.keras/datasets/VOC2012/combined_annotations')
-        data_suffix='.jpg'
-        label_suffix='.png'
-        classes = 21
-    if dataset == 'COCO':
+        #train_file_path = os.path.expanduser('~/.keras/datasets/VOC2012/combined_imageset_train.txt') #Data/VOClarge/VOC2012/ImageSets/Segmentation
+#        train_file_path = os.path.expanduser('~/.keras/datasets/VOC2012/VOCdevkit/VOC2012/ImageSets/Segmentation/train.txt') #Data/VOClarge/VOC2012/ImageSets/Segmentation
+#        val_file_path   = os.path.expanduser('~/.keras/datasets/VOC2012/combined_imageset_val.txt')
+#        data_dir        = os.path.expanduser('~/.keras/datasets/VOC2012/VOCdevkit/VOC2012/JPEGImages')
+#        label_dir       = os.path.expanduser('~/.keras/datasets/VOC2012/combined_annotations')
+#        data_suffix='.jpg'
+#        label_suffix='.png'
+#        classes = 21
+#    if dataset == 'COCO':
         # ###################### loss function & metric ########################
-        train_file_path = os.path.expanduser('~/.keras/datasets/VOC2012/VOCdevkit/VOC2012/ImageSets/Segmentation/train.txt') #Data/VOClarge/VOC2012/ImageSets/Segmentation
+#        train_file_path = os.path.expanduser('~/.keras/datasets/VOC2012/VOCdevkit/VOC2012/ImageSets/Segmentation/train.txt') #Data/VOClarge/VOC2012/ImageSets/Segmentation
         # train_file_path = os.path.expanduser('~/.keras/datasets/oneimage/train.txt') #Data/VOClarge/VOC2012/ImageSets/Segmentation
-        val_file_path   = os.path.expanduser('~/.keras/datasets/VOC2012/VOCdevkit/VOC2012/ImageSets/Segmentation/val.txt')
-        data_dir        = os.path.expanduser('~/.keras/datasets/VOC2012/VOCdevkit/VOC2012/JPEGImages')
-        label_dir       = os.path.expanduser('~/.keras/datasets/VOC2012/VOCdevkit/VOC2012/SegmentationClass')
-        loss_fn = binary_crossentropy_with_logits
-        metrics = [binary_accuracy]
-        loss_shape = (target_size[0] * target_size[1] * classes,)
-        label_suffix = '.npy'
-        data_suffix='.jpg'
-        ignore_label = None
-        label_cval = 0
+#        val_file_path   = os.path.expanduser('~/.keras/datasets/VOC2012/VOCdevkit/VOC2012/ImageSets/Segmentation/val.txt')
+#        data_dir        = os.path.expanduser('~/.keras/datasets/VOC2012/VOCdevkit/VOC2012/JPEGImages')
+#        label_dir       = os.path.expanduser('~/.keras/datasets/VOC2012/VOCdevkit/VOC2012/SegmentationClass')
+#        loss_fn = binary_crossentropy_with_logits
+#        metrics = [binary_accuracy]
+#        loss_shape = (target_size[0] * target_size[1] * classes,)
+#        label_suffix = '.npy'
+#        data_suffix='.jpg'
+#        ignore_label = None
+#        label_cval = 0
 
 
     # ###################### loss function & metric ########################
-    if dataset == 'VOC2012' or dataset == 'VOC2012_BERKELEY':
-        loss_fn = softmax_sparse_crossentropy_ignoring_last_label
-        metrics = [sparse_accuracy_ignoring_last_label]
-        loss_shape = None
-        ignore_label = 255
-        label_cval = 255
+    #if dataset == 'VOC2012' or dataset == 'VOC2012_BERKELEY':
+    loss_fn = softmax_sparse_crossentropy_ignoring_last_label
+    metrics = [sparse_accuracy_ignoring_last_label]
+    loss_shape = None
+    ignore_label = 255
+    label_cval = 255
 
     # Class weight is not yet supported for 3+ dimensional targets
     # class_weight = {i: 1 for i in range(classes)}
@@ -220,7 +286,10 @@ if __name__ == '__main__':
     config = tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True))
     session = tf.Session(config=config)
     K.set_session(session)
-    train(batch_size, epochs, lr_base, lr_power, weight_decay, classes, model_name, train_file_path, val_file_path,
-          data_dir, label_dir, target_size=target_size, batchnorm_momentum=batchnorm_momentum, resume_training=resume_training,
-          class_weight=class_weight, loss_fn=loss_fn, metrics=metrics, loss_shape=loss_shape, data_suffix=data_suffix,
-          label_suffix=label_suffix, ignore_label=ignore_label, label_cval=label_cval)
+
+    
+    
+    train(conf.batch_size, conf.epochs, lr_base, lr_power, weight_decay, conf.classes, conf.model_name, conf.train_file_path, conf.val_file_path,
+          conf.data_dir, conf.label_dir, target_size=target_size, batchnorm_momentum=batchnorm_momentum, load_model=conf.load_model,
+          class_weight=class_weight, loss_fn=loss_fn, metrics=metrics, loss_shape=loss_shape, data_suffix=conf.data_suffix,
+          label_suffix=conf.label_suffix, ignore_label=ignore_label, label_cval=label_cval)
